cb4cd80047e418800411bca6159654186fcda0ec
Eunjoo-Jung <jejrabbit@naver.com>
2024-10-30 21:49:48 +0900

    241030

--- 241022/로블록스/script.js
+++ 241022/로블록스/script.js
@@ -1,13 +1,22 @@
-const femaleBtn = document.getElementById("FemaleBtn");
+const femaleBtn = document.getElementById("femaleBtn");
 femaleBtn.addEventListener("click", (e) => {
   e.preventDefault();
   document.querySelector("#femaleBtn i").classList.add("filledA");
-  document.querySelector("#femaleBtn i").classList.remove("filledB");
+  document.querySelector("#maleBtn i").classList.remove("filledB");
 });
 
-const maleBtn = document.getElementById("maleBtn");
-maleBtn.querySelector("click", (e) => {
+// const maleBtn = document.getElementById("maleBtn");
+// console.log(maleBtn)
+// maleBtn.querySelector("click", (e) => {
+//   e.preventDefault();
+//   console.log("test")
+//   // document.querySelector("#maleBtn i").classList.add("filledB");
+//   // document.querySelector("#femaleBtn i").classList.remove("filledA");
+// });
+
+const maleBtn = document.querySelector("#maleBtn");
+maleBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  document.querySelector("maleBtn i").classList. add("filledB");
-  document.querySelector("#femaleBtn i").classList.remove("filledA");
+document.querySelector("#maleBtn i").classList.add("filledB");
+document.querySelector("#femaleBtn i").classList.remove("filledA");
 });

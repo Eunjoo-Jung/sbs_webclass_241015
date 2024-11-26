2d490279adc0a2b4d530895c8286d33c497bd77b
Eunjoo-Jung <jejrabbit@naver.com>
2024-11-26 21:46:31 +0900

    241126

--- web02/241125/script.js
+++ web02/241125/script.js
@@ -1,7 +1,46 @@
-window.addEventListener("scroll", ()=> {
-  if(window.scrollY > 40) {
-    document.querySelector("header").classList.add("active");
+// scroll event
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("active");
   } else {
-    document.querySelector("header").classList.remove("active");
+    header.classList.remove("active");
   }
+});
+
+// slick 
+$('.responsive').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
